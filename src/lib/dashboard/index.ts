b2efa267553d2be037aeff509c91879ce68700b7
Test <test@test.com>
2026-03-07 10:41:35 -0300

    initial

--- src/lib/dashboard/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Dashboard Module
- * Declarative dashboards for terminal visualization
- */
-
-export * from './types.js';
-export * from './loader.js';
-export * from './engine.js';
-export { postgresSource } from './sources/postgres.js';
